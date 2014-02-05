6b9269db4d1ae6b2f9e3d303448d9150728e0fee
sseppola <sseppola@me.com>
2014-02-05 22:43:31 +1100

    Added priority listing of tasks using classes

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,18 +1,38 @@
 if (Tasks.find().count() === 0) {
 	
 	Tasks.insert({
-		title: 'Write masterpiece',
+		title: 'Make checkboxes',
+		priority: 1,
+		completed: true
+	});
+
+	Tasks.insert({
+		title: 'Collapse tasks > 3',
+		priority: 2,
 		completed: false
 	});
 
 	Tasks.insert({
-		title: 'Work out',
+		title: 'Create object that contains today\'s tasks',
+		priority: 3,
 		completed: false
 	});
 
 	Tasks.insert({
-		title: 'Dance',
+		title: 'Create floated footer bar with expandable projects',
+		priority: null,
+		completed: false
+	})
+
+	Tasks.insert({
+		title: 'Enable drag & drop',
+		priority: null,
 		completed: false
 	});
-	
+
+	Tasks.insert({
+		title: 'Make app adjust to timezones',
+		priority: null,
+		completed: false
+	});
 }
