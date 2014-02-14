b65e05a6baeb22a94cc43f5bf7d94cc86b19cb0d
sseppola <sseppola@me.com>
2014-02-14 20:55:15 +1100

    Drawer now keeps state on code refreshes, and animates when opening/closing

--- client/views/drawer/projects_drawer.js
+++ client/views/drawer/projects_drawer.js
@@ -1,12 +1,34 @@
+Template.projectsDrawer.helpers({
+	// drawerState: function () {
+	// 	if (!Session.get('drawerState'))
+	// 		Session.set('drawerState', 'closed');
+
+	// 	return Session.get('drawerState');
+	// }
+});
+
 Template.projectsDrawer.events({
 	'click .tag': function () {
 		var $d = $('.drawer');
 		if ($d.hasClass('open')) {
-			console.log("closing");
+			Session.set('drawerState', 'closed');
 			$d.removeClass('open').addClass('closed');
 		} else {
-			console.log("opening");
+			Session.set('drawerState', 'open');
 			$d.removeClass('closed').addClass('open');
 		}
+		
 	}
 });
+
+Template.projectsDrawer.rendered = function () {
+	var state = Session.get('drawerState');
+	if (!state)
+		Session.set('drawerState', 'closed');
+
+	$('.drawer').removeClass('open closed').addClass(Session.get('drawerState'));
+};
+
+
+
+
